8a606322ac5abb98a62e1cbad6677107bf7f0823
Michelle Aklufi <103528493+maklufi@users.noreply.github.com>
2022-05-18 01:55:39 +0000

    finished

--- part2.py
+++ part2.py
@@ -8,5 +8,6 @@ The formula for converting from fahrenheit to celsius:
 C = (F - 32) * 5/9
 """
 
-def celsius(f):  # do not change this line
-  pass  # delete the word pass when you start writing your code
+def celsius(f):
+  c = (f - 32) * 5/9
+  return c
--- part3.py
+++ part3.py
@@ -18,5 +18,6 @@ def factorial(number):
     product *= i
   return product
 
-def combination(n, r):  # do not change this line
-  pass  # delete the word pass when you start writing your code
+def combination(n, r):
+  c = factorial(n) / factorial(r) * factorial(n-r)
+  return c
